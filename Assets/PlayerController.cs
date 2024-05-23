e833b60f34bcd1b6e930c8f2e259d0d13bc0f4a7
Tom Consumes from Nutrition Impossible <146956075+x3emmi@users.noreply.github.com>
2024-05-23 08:47:39 -0400

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Player.AddForce(Vector3.up * 10, ForceMode2D.Impulse);
+        }
+    
+    }
+}
